f3f1b2dbfe51476e8b98ca6e17f17ce02db7bf65
Tadeáš Kmenta <TheTrunk@users.noreply.github.com>
2017-11-27 00:24:27 +0100

    Initial

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-# myzenwallet
+# myhushwallet
 
-MyZENWallet is a client-side browser-based wallet for ZenCASH.
+MyHushWallet is a client-side browser-based wallet for Hush.
+
+This project is a port of MyZenWallet https://github.com/ZencashOfficial/myzenwallet which was inspired by MyEtherWallet.
+
+Currently avaiable at: https://wallet.myhush.network
 
 # Running locally
-Download one of the releases, goto the `dist` folder and double click `index.html`
+Download index.html, faq.html, guide.html, favicon.ico, Javascript folder (js) and double click `index.html`
+
 
 # Dev
 ```shell
--- app/components/faq.jsx
+++ app/components/faq.jsx
@@ -14,23 +14,19 @@ export default class ZFaq extends React.Component {
 
             <p>
               <b>Q: How is this secure? Its on a web browser!</b><br/>
-              A: MyZenWallet has replicated the core features of the ZEN daemon in JavaScript! So the process of creating and signing the transactions are all done within the browser. No sensitive information is sent through the network.
+              A: My Hush Wallet has replicated the core features of the HUSH daemon in JavaScript! So the process of creating and signing the transactions are all done within the browser. No sensitive information is sent through the network.
             </p>         
 
             <p>
               <b>Q: Can I have the source code?</b><br/>
-              A: <a href="https://github.com/zencashofficial/myzenwallet">Here you go</a>
+              A: <a href="https://github.com/myhush/myhushwallet">Here you go</a>
             </p>               
 
             <p>
               <b>Q: Why are you doing this?</b><br/>
-              A: MyZenWallet was inspired by <a href="https://myetherwallet.com">MyEtherWallet's</a> mission statement: to provide the people the ability to interact with the ZEN blockchain easily, without having to run a full node.
+              A: My Hush Wallet was inspired by <a href="https://myzenwallet.io/">MyZenWallet's</a> which was inspired by <a href="https://myetherwallet.com">MyEtherWallet's.</a> We also want to provide the people the ability to interact with the HUSH blockchain easily, without having to run a full node.
             </p>
-
-            <p>
-              <b>Q: Can I get free ZEN?</b><br/>
-              A: Sure! Select an address frm your wallet (check out how to create a wallet via <a href="/guide.html">the guide</a> if you don't have one yet) and enter it into <a href="http://getzen.cash">getzen.cash</a>.
-            </p>           
+          
           </Col>
         </Row>
       </Container>
